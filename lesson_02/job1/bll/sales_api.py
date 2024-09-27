2d19c8294c66130c95e784642499194ad1709afd
SafonovVladimir <vladimir_safonov@ukr.net>
2024-09-27 14:40:24 +0300

    implement save_sales_to_local_disk function

--- lesson_02/job1/bll/sales_api.py
+++ lesson_02/job1/bll/sales_api.py
@@ -1,10 +1,7 @@
-from Lecture_2.ht_template.job1.dal import local_disk, sales_api
+from lesson_02.job1.dal.local_disk import save_to_disk
+from lesson_02.job1.dal.sales_api import get_sales
 
 
 def save_sales_to_local_disk(date: str, raw_dir: str) -> None:
-    # TODO: implement me
-    # 1. get data from the API
-    # 2. save data to disk
-
-    print("\tI'm in get_sales(...) function!")
-    pass
+    content = get_sales(date)
+    save_to_disk(date, content, raw_dir)
