03b6519f02afa959c964ad10388f930d716f4b9d
SafonovVladimir <vladimir_safonov@ukr.net>
2024-09-27 12:05:39 +0300

    add create_or_clean_is_exists_directory function and check is exist data

--- lesson_02/job1/dal/local_disk.py
+++ lesson_02/job1/dal/local_disk.py
@@ -5,11 +5,8 @@ from typing import List, Dict, Any
 
 from lesson_02.job1.dal.sales_api import get_sales
 
-date = "2022-08-11"
-content = get_sales(date)
 
-
-def remove_all_files_from_directory(directory):
+def remove_all_files_from_directory(directory: Path) -> None:
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
         try:
@@ -18,31 +15,36 @@ def remove_all_files_from_directory(directory):
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
         except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+            print("Failed to delete %s. Reason: %s" % (file_path, e))
+
+
+def create_or_clean_is_exists_directory(json_content: bool, path: str) -> None:
+    work_directory = Path(os.getcwd()).parents[1]
+    suffix_path = Path(path)
+    storage_directory = Path(work_directory, "file_storage", suffix_path, date)
+
+    if json_content:
+        os.makedirs(storage_directory)
+        remove_all_files_from_directory(storage_directory)
+        # os.makedirs(storage_directory)
+        os.chdir(storage_directory)
 
 
 def save_to_disk(json_content: List[Dict[str, Any]], path: str) -> None:
     if json_content:
-        lesson_directory = Path(os.getcwd()).parents[1]
-        suffix_path = Path(path)
-        storage_directory = Path(lesson_directory, suffix_path, date)
-
+        create_or_clean_is_exists_directory(True, path)
         file_number = 1
-
-        if os.path.exists(storage_directory):
-            remove_all_files_from_directory(storage_directory)
-        else:
-            print(f"All records for {date} have been added to local storage.")
-            os.makedirs(storage_directory)
-
-        os.chdir(storage_directory)
-
         for record in json_content:
             with open(f"sales_{date}_{file_number}", "w") as file:
                 file.write(str(record))
             file_number += 1
+        print(f"All records for {date} have been added to local storage.")
     else:
+        create_or_clean_is_exists_directory(False, path)
         print("There are no sales records for this date!")
 
 
-save_to_disk(content, "file_storage/raw/sales")
+date = "2022-08-04"
+content = get_sales(date)
+
+save_to_disk(content, "raw/sales")
