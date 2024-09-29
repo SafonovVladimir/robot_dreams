253ba61d18ae6a8e2c36cc7c6b08f858c6dd827e
SafonovVladimir <vladimir_safonov@ukr.net>
2024-09-29 09:02:51 +0300

    add 2 query

--- lesson_03/home_task_queries.sql
+++ lesson_03/home_task_queries.sql
@@ -9,27 +9,28 @@
 Результат відсортувати за спаданням.
 */
 
-SELECT
-    category.name AS category_name,
-    COUNT(film.film_id) AS film_count
-FROM
-    category
-LEFT JOIN
-    film_category ON category.category_id = film_category.category_id
-LEFT JOIN
-    film ON film_category.film_id = film.film_id
-GROUP BY
-    category.name
-ORDER BY
-    film_count DESC;
+SELECT category.name AS category_name, COUNT(film.film_id) AS film_count
+FROM category
+LEFT JOIN film_category ON category.category_id = film_category.category_id
+LEFT JOIN film ON film_category.film_id = film.film_id
+GROUP BY category.name
+ORDER BY film_count DESC;
 
 /*
 2.
 Вивести 10 акторів, чиї фільми брали на прокат найбільше.
 Результат відсортувати за спаданням.
 */
--- SQL code goes here...
-
+SELECT CONCAT(actor.first_name, ' ', actor.last_name) AS actor_full_name,
+       COUNT(rental.rental_id) AS rental_count
+FROM actor
+JOIN film_actor ON actor.actor_id = film_actor.actor_id
+JOIN film ON film_actor.film_id = film.film_id
+JOIN inventory ON film.film_id = inventory.film_id
+JOIN rental ON inventory.inventory_id = rental.inventory_id
+GROUP BY actor.actor_id
+ORDER BY rental_count DESC
+LIMIT 10;
 
 
 /*
