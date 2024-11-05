b027833132fdadad2033d239a4f61c5cc46d1b6e
SafonovVladimir <vladimir_safonov@ukr.net>
2024-11-05 16:35:59 +0200

    remove lesson 10

--- lesson_10/dags/settings/constants.py
+++
@@ -1,6 +0,0 @@
-# Define bucket and connection ID
-BUCKET = "robot-dreams-lecture-10-hw"
-GCP_CONN_ID = "google_cloud_default"
-
-# Set the root directory containing the folders and files
-LOCAL_ROOT_DIRECTORY = "/opt/airflow/file_storage/stg/sales/"
