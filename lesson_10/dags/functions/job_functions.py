ecf659625b48fbb988cbd2b20be5d67e53c71714
SafonovVladimir <vladimir_safonov@ukr.net>
2024-11-06 15:20:11 +0200

    add DAG upload to Amazon S3 bucket

--- lesson_10/dags/functions/job_functions.py
+++ lesson_10/dags/functions/job_functions.py
@@ -3,15 +3,14 @@ import os
 from airflow.models import Variable
 
 
-# Function to recursively get all files with their relative paths
+# Function to recursively get all files
 def get_files_with_relative_paths(root_dir):
-    file_paths = []
+    files_list = []
     date_str = Variable.get("upload_date")
     for dirpath, _, filenames in os.walk(root_dir):
         for filename in filenames:
             full_path = os.path.join(dirpath, filename)
-            # Get the relative path for GCS (preserve folder structure)
             relative_path = os.path.relpath(full_path, root_dir)
             if relative_path.split('/')[-2] == date_str:
-                file_paths.append(full_path)
-    return file_paths
+                files_list.append(full_path)
+    return files_list
--- lesson_10/dags/process_sales.py
+++ lesson_10/dags/process_sales.py
@@ -2,11 +2,12 @@ import os
 
 from airflow import DAG
 from airflow.models import Variable
+from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime
 
-from settings.constants import BUCKET, GCP_CONN_ID, LOCAL_ROOT_DIRECTORY
+from settings.constants import GCP_BUCKET, GCP_CONN_ID, LOCAL_ROOT_DIRECTORY, S3_BUCKET, AWS_S3_CONN_ID
 from functions.job_functions import get_files_with_relative_paths
 
 default_args = {
@@ -21,19 +22,39 @@ formatted_date = date.strftime("%Y/%m/%d")
 
 files_to_upload = get_files_with_relative_paths(LOCAL_ROOT_DIRECTORY)
 
+# Define the DAG GCP
 with DAG(
         "upload_files_to_gcs",
         default_args=default_args,
-        schedule_interval=None,
+        schedule_interval='@once',
         start_date=days_ago(1),
+        catchup=False,
 ) as dag:
     # Task to upload files
-    for file_path in files_to_upload:
+    for file in files_to_upload:
         gcs_destination = f"src1/sales/v1/{formatted_date}/"
         upload_task = LocalFilesystemToGCSOperator(
-            task_id=f"upload_{os.path.basename(file_path)}",
-            src=file_path,
+            task_id=f"upload_{os.path.basename(file)}",
+            src=file,
             dst=gcs_destination,
-            bucket=BUCKET,
+            bucket=GCP_BUCKET,
             gcp_conn_id=GCP_CONN_ID,
+        )
+
+# Define the DAG S3
+with DAG(
+        'upload_files_to_s3',
+        default_args=default_args,
+        start_date=days_ago(1),
+        schedule_interval='@once',
+        catchup=False,
+) as dag:
+    for file in files_to_upload:
+        s3_uri = f"s3://{S3_BUCKET}/src1/sales/v1/{formatted_date}/{file.split('/')[-1]}"
+        upload_file_to_s3 = LocalFilesystemToS3Operator(
+            task_id=f"upload_{os.path.basename(file)}",
+            filename=file,
+            dest_key=s3_uri,
+            aws_conn_id=AWS_S3_CONN_ID,
+            replace=True,  # Replace existing file
         )
