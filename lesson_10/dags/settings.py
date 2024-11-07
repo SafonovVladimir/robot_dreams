4f768e906e1a13d10dea034dea815182e3d91133
SafonovVladimir <vladimir_safonov@ukr.net>
2024-11-07 12:32:44 +0200

    use constant UPLOAD_DATE from constants

--- lesson_10/dags/settings.py
+++ lesson_10/dags/settings.py
@@ -1,7 +1,6 @@
-from airflow.models import Variable
 from datetime import datetime
 
-from constants import LOCAL_ROOT_DIRECTORY
+from constants import LOCAL_ROOT_DIRECTORY, UPLOAD_DATE
 from functions.job_functions import get_files_with_relative_paths
 
 default_args = {
@@ -10,8 +9,7 @@ default_args = {
 }
 
 # Variable name should be "upload_date"
-date_str = Variable.get("upload_date")
-date = datetime.strptime(date_str, "%Y-%m-%d")
+date = datetime.strptime(UPLOAD_DATE, "%Y-%m-%d")
 formatted_date = date.strftime("%Y/%m/%d")
 
 files_to_upload = get_files_with_relative_paths(LOCAL_ROOT_DIRECTORY)
