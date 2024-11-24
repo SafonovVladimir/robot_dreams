3835b253ee52cf5a8112a82f5a9ae46771bea97b
SafonovVladimir <vladimir_safonov@ukr.net>
2024-11-24 13:18:04 +0200

    Add trigger_enrich_user_profiles to process_user_profiles DAG

--- lesson_17/dags/process_user_profiles.py
+++ lesson_17/dags/process_user_profiles.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from airflow import DAG
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 
 from constants import BUCKET_NAME, USER_PROFILES_PATH, USER_PROFILES_SILVER_DATASET_TABLES
@@ -19,3 +20,12 @@ with DAG(
         source_format="NEWLINE_DELIMITED_JSON",
         write_disposition="WRITE_TRUNCATE",
     )
+
+    trigger_enrich_user_profiles = TriggerDagRunOperator(
+        task_id="trigger_enrich_user_profiles",
+        trigger_dag_id="enrich_user_profiles",
+        reset_dag_run=True,
+        wait_for_completion=True
+    )
+
+    load_to_silver >> trigger_enrich_user_profiles  # Set the dependency
