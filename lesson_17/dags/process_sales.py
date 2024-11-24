eab17a9a772c523d320524dd28531dfe00258c12
SafonovVladimir <vladimir_safonov@ukr.net>
2024-11-24 13:55:24 +0200

    Change type CustomerId to INT64

--- lesson_17/dags/process_sales.py
+++ lesson_17/dags/process_sales.py
@@ -75,7 +75,7 @@ with DAG(
             CREATE OR REPLACE TABLE sep2024-volodymyr-safonov.silver.sales
             PARTITION BY purchase_date AS
             SELECT
-                SAFE_CAST(CustomerId AS STRING) AS client_id,
+                SAFE_CAST(CustomerId AS INT64) AS client_id,
                 SAFE.PARSE_DATE("%Y-%m-%d", PurchaseDate) AS purchase_date,
                 SAFE_CAST(Product AS STRING) AS product_name,
                 SAFE_CAST(REGEXP_REPLACE(Price, r"[^0-9.]", "") AS FLOAT64) AS price
